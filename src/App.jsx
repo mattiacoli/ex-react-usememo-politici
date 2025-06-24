4e1ddc12bf7666f39291780e805c37a2e7db6648
mattiacoli <mattia.coli@aol.com>
2025-06-24 13:00:05 +0200

    Milestone 3 edit card component using React.memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState, useMemo, memo } from 'react'
+
+const Card = memo(({ p }) => {
+
+  console.log('componente renderizzato', p.name);
+
+  return (
+    <div className="col">
+      <div className="card h-100">
+        <div className="card-header">
+          <h2>{p.name}</h2>
+        </div>
+        <div className="card-body">
+          <p className='fw-bold'>{p.position}</p>
+          <p>{p.biography}</p>
+        </div>
+      </div>
+    </div>
+  )
+})
+
 
 function App() {
 
@@ -39,17 +59,7 @@ function App() {
       <div className="container">
         <div className="row row-cols-3 gy-4">
           {filteredPoliticians.map(item => (
-            <div key={item.id} className="col">
-              <div className="card h-100">
-                <div className="card-header">
-                  <h2>{item.name}</h2>
-                </div>
-                <div className="card-body">
-                  <p className='fw-bold'>{item.position}</p>
-                  <p>{item.biography}</p>
-                </div>
-              </div>
-            </div>
+            <Card key={item.id} p={item} />
           ))}
         </div>
       </div>
